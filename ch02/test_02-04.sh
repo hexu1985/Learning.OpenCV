9a624717d351782832f96eb74cd753279cd256d8
hexu <hexu@sensetime.com>
2019-07-08 14:05:41 +0800

    add v2 and v3 sub dir

--- ch02/test_02-04.sh
+++ ch02/test_02-04.sh
@@ -1,2 +1,7 @@
-./example_02-04 ../tree.avi
-./example_02-04 ../test.avi
+./${SUB}/example_02-04 ../tree.avi
+./${SUB}/example_02-04 ../test.avi
+
+# 变量SUB取v2或v3, 调用方式为
+# SUB=v2 bash test_02-04.sh
+# 或者
+# SUB=v3 bash test_02-04.sh
