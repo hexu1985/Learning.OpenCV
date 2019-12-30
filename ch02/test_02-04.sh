8bbde7fcbff58ce8c7948d382625b8f66ca1281f
hexu <hexu_1985@sina.com>
2019-12-30 17:10:20 +0800

    add build_msys.sh

--- ch02/test_02-04.sh
+++ ch02/test_02-04.sh
@@ -1,7 +1,2 @@
-./${SUB}/example_02-04 ../tree.avi
-./${SUB}/example_02-04 ../test.avi
-
-# 变量SUB取v2或v3, 调用方式为
-# SUB=v2 bash test_02-04.sh
-# 或者
-# SUB=v3 bash test_02-04.sh
+./example_02-04 ../tree.avi
+./example_02-04 ../test.avi
--- ch02/test_02-11.sh
+++ ch02/test_02-11.sh
@@ -1,7 +1,2 @@
-./${SUB}/example_02-11 ../test.avi ../vout.avi
-./${SUB}/example_02-11 ../tree.avi ../vout2.avi
-
-# 变量SUB取v2或v3, 调用方式为
-# SUB=v2 bash test_02-11.sh
-# 或者
-# SUB=v3 bash test_02-11.sh
+./example_02-11 ../test.avi ../vout.avi
+./example_02-11 ../tree.avi ../vout2.avi
